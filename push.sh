d033b0b2a723fb1d3d4f87795c6d3d44a57e0d12
matt.b.carson <matt.b.carson@CPX-92Q1JGPFJNE.dir.svc.accenture.com>
2017-02-16 13:44:02 -0700

    standard fixes

---
+++ push.sh
@@ -0,0 +1,12 @@
+docker push mcarsonacn/hystrix-dashboard
+docker push mcarsonacn/shopping-cart-service
+docker push mcarsonacn/edge-service
+docker push mcarsonacn/catalog-service
+docker push mcarsonacn/online-store-web
+docker push mcarsonacn/discovery-service
+docker push mcarsonacn/config-service
+docker push mcarsonacn/payment-service
+docker push mcarsonacn/user-service
+docker push mcarsonacn/account-service
+docker push mcarsonacn/order-service
+docker push mcarsonacn/inventory-service
